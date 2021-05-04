aed7f7a338da2c953b328d28591b5d416f8b94c5
guolongfei <18255599540@163.com>
2021-05-04 12:31:45 +0800

    hello

---
+++ helloworld/src/main/java/com/hello/HelloWor.java
@@ -0,0 +1,19 @@
+package com.hello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Auther: guo
+ * @Date: 12:22 2021/5/4
+ */
+public class HelloWor {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list.toString());
+
+    }
+}
